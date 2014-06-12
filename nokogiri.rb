00bc0bcf7af90019d56956dd8896ed571d28bba7
thakkarjinal <thakkarjinal29@gmail.com>
2014-06-12 20:10:10 +0530

    changes done

--- nokogiri.rb
+++ nokogiri.rb
@@ -2,19 +2,28 @@ require 'rubygems'
 require 'nokogiri' 
 require 'open-uri'
 
-puts "Enter the number of movies"
-no_of_movies = gets.chomp.to_i
+while 1
+	puts "Enter the number of movies"
+	no_of_movies = gets.chomp.to_i
+	if no_of_movies > 250
+		puts "The number should not exceed 250"
+		next
+	else 
+
+		break
+	end
+end
 
 link = "http://www.imdb.com/chart/top?ref_=nv_ch_250_4"
 doc = Nokogiri::HTML(open(link))
 hash = Hash.new ([])
 movie_list = doc.css('tbody.lister-list tr td.titleColumn a')
 for movie in movie_list
-	doc_new = Nokogiri::HTML(open("http://www.imdb.com"+movie.attribute("href")))
+	doc_new = Nokogiri::HTML(open("http://www.imdb.com" + movie.attribute("href")))
 	stars =  doc_new.css('table.cast_list tr td.itemprop a')
 		star_array=[]
 		for star in stars
-			star_array << star.text.strip
+			star_array << star.text.strip.downcase
 		end
 	hash[movie.text] = star_array
 end
